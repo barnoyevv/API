0999f3a59682d1509ff838488d42cb03b32f2c7f
Shohjahon <shohjahon@MacBook-Air-Shohjahon.local>
2024-06-04 15:14:50 +0500

    first commit

--- src/pages/photos/index.jsx
+++ src/pages/photos/index.jsx
@@ -1,10 +1,44 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
 
 const Photos = () => {
-	return (
-		<div>
-			<h1>photos</h1>
-		</div>
-	)
+	const [users,setUsers] = useState([])
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/photos").then(response=>{
+      console.log(response)
+      setUsers(response.data)
+    })
+  },[])
+  return (
+    <div className='users'>
+      <h1 className='text-center my-3'>Photos</h1>
+     <table className='table table-bordered table-hover table-striped'>
+      <thead>
+        <tr>
+          <th>AlbumID</th>
+          <th>ID</th>
+          <th>Title</th>
+          <th>URL</th>
+          <th>ThumbnailUrl</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          users.map((item,index)=>(
+            <tr key={index}>
+            <td>{item.albumId}</td>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.url}</td>
+            <td>{item.thumbnailUrl}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+     </table>
+    </div>
+  )
 }
 
 export default Photos
